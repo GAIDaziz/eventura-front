11d81fba04a1a13873cc227a25c25a9ad68564e2
GAIDaziz <gaidaz@hotmail.com>
2025-01-20 14:48:51 +0100

    modification header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
-      <nav>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">À propos</Link></li>
-          <li><Link to="/create-event">Créer un évènement</Link></li>
-          <li><Link to="/signup">Inscription</Link></li>
-          <li><Link to="/login">Connexion</Link></li>
-        </ul>
-      </nav>
+      <div className="navbar-container">
+        <div className="logo">Eventura</div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <i className={menuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <nav className={menuOpen ? 'nav-menu active' : 'nav-menu'}>
+          <ul>
+            <li>
+              <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+            </li>
+            <li>
+              <Link to="/create-event" onClick={() => setMenuOpen(false)}>Create Event</Link>
+            </li>
+            <li>
+              <Link to="/signup" className="btn signup-btn" onClick={() => setMenuOpen(false)}>Inscription</Link>
+            </li>
+            <li>
+              <Link to="/login" className="btn login-btn" onClick={() => setMenuOpen(false)}>Connexion</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
